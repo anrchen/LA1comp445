12e39f8abe2d15f26e248c4576523bd16920fbb1
Tyler Ramsay <starvator.ty@gmail.com>
2017-01-30 13:33:52 -0500

    removing yargs dependency

--- client.js
+++ client.js
@@ -1,22 +1,22 @@
 'use strict';
 
 const net = require('net');
-const yargs = require('yargs');
-    var url = require('url');
+//const yargs = require('yargs');
+var url = require('url');
 
-const argv = yargs.usage('node client.js (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL')
+/*const argv = yargs.usage('node client.js (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL')
     .default('host', 'httpbin.org')
     .default('port', 80)
     .default('path', '/get?course=networking&assignment=1')
     .default('get', 1)
     .help('help')
-    .argv;
+    .argv;*/
 
 //if help only
 if (process.argv[2] == 'help') {
     var printdefaultHelp = true;
     if (process.argv.length == 4) {
-        switch (process.argv[3]) {
+        switch (process.argv[3].toLowerCase()) {
         case "get":
             printdefaultHelp = false;
             console.log('usage: httpc get [-v] [-h key:value] URL');
@@ -56,42 +56,41 @@ if (process.argv[2] == 'help') {
     process.exit(-1);
 }
 
+if (process.argv.length > 3) {
+    //handle get
+    var isGet;
+    if (process.argv[2].toLowerCase() == "get") {
+        isGet = true;
+        var type = "GET";
+        var verbose = (process.argv[3].toLowerCase() == "-v");
+        var additHeaderParams = "";
+        //parse the URL
+        var urlToParse = process.argv[process.argv.length - 1];
+        var urlObj = url.parse(urlToParse);
 
-//handle get
-if (process.argv.length>3){
-if (process.argv[2].toLowerCase() == "get") {
-    console.log('node client.js get [-v] [-h key:value] URL');
-    var type = "GET";
-    var verbose = (process.argv[3].toLowerCase() == "-v");
-    var additHeaderParams = "";
-    //parse the URL
-    var urlToParse = process.argv[process.argv.length - 1];
-    var urlObj = url.parse(urlToParse);
-
-    //handle the remaining -h
-    var numberParamsNoH = 4;
-    if (verbose) {
-        numberParamsNoH++;
-    }
-    //if there is not an even humber of [-h key:value] left, quit
-    if ((process.argv.length - numberParamsNoH) % 2 != 0) {
-        console.log("An invalid number of parameters was passed.")
-        process.exit(-1);
-    }
-    
-    //process the -h*
-    while (process.argv.length - numberParamsNoH > 0) {
-        if (process.argv[numberParamsNoH - 1].toLowerCase() != "-h") {
-            console.log("An invalid pair was specified in the -h key:value Associates headers.")
+        //handle the remaining -h
+        var numberParamsNoH = 4;
+        if (verbose) {
+            numberParamsNoH++;
+        }
+        //if there is not an even humber of [-h key:value] left, quit
+        if ((process.argv.length - numberParamsNoH) % 2 != 0) {
+            console.log("An invalid number of parameters was passed.")
             process.exit(-1);
         }
-        additHeaderParams = additHeaderParams + process.argv[numberParamsNoH] + "\n";
-        numberParamsNoH++;
-        numberParamsNoH++;
+
+        //process the -h*
+        while (process.argv.length - numberParamsNoH > 0) {
+            if (process.argv[numberParamsNoH - 1].toLowerCase() != "-h") {
+                console.log("An invalid pair was specified in the -h key:value Associates headers.")
+                process.exit(-1);
+            }
+            additHeaderParams = additHeaderParams + process.argv[numberParamsNoH] + "\n";
+            numberParamsNoH++;
+            numberParamsNoH++;
+        }
+        var clientWritingString = type + " " + urlObj.path + " HTTP/1.0 \nHost: " + urlObj.host + "\n" + additHeaderParams + "\n";
     }
-    var clientWritingString = type+" " + urlObj.path + " HTTP/1.0 \nHost: " + urlObj.host + "\n"+ additHeaderParams +"\n";
-    console.log(clientWritingString);
-}
 } else {
     console.log("params busted");
     process.exit(-1);
@@ -108,20 +107,20 @@ for (var proc in process.argv) {
 console.log(process.argv);
 */
 const client = net.createConnection({
-    host: argv.host,
-    port: argv.port
+    host: urlObj.host,
+    port: 80
 });
 
 client.on('data', buf => {
-    console.log("-----DATA RECEIVED BACK");
+    //TODO: -v verbose
     console.log(buf.toString());
-    console.log("-----DONE");
+
 });
 
 client.on('connect', () => {
-    if (argv.get == 1) {
+    if (isGet) {
         client.write(clientWritingString);
-       // client.write("GET " + argv.path + " HTTP/1.0 \n Host: " + argv.host + "\naaaaa: bbbbb\n\n");
+        // client.write("GET " + argv.path + " HTTP/1.0 \n Host: " + argv.host + "\naaaaa: bbbbb\n\n");
     } else {
         client.write("POST /post HTTP/1.0\nHost: httpbin.org\ncontent-type: application/json\naaaaa: bbbbb\ncontent-length: 23\n\n{\"mapName\":\"myMapName\"}\n");
     }
