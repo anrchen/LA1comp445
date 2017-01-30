72291369b651e700064f93f47c196e3af99939ee
Tyler Ramsay <starvator.ty@gmail.com>
2017-01-30 12:47:32 -0500

    adding all the help and starting to work on the params for  GET

--- client.js
+++ client.js
@@ -3,7 +3,7 @@
 const net = require('net');
 const yargs = require('yargs');
 
-const argv = yargs.usage('node client.js [--host host] [--port port]')
+const argv = yargs.usage('node client.js (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL')
     .default('host', 'httpbin.org')
     .default('port', 80)
     .default('path', '/status/418')
@@ -11,23 +11,113 @@ const argv = yargs.usage('node client.js [--host host] [--port port]')
     .help('help')
     .argv;
 
-const client = net.createConnection({host: argv.host, port: argv.port});
+//if help only
+if (process.argv[2] == 'help') {
+    var printdefaultHelp = true;
+    if (process.argv.length == 4) {
+        switch (process.argv[3]) {
+        case "get":
+            printdefaultHelp = false;
+            console.log('usage: httpc get [-v] [-h key:value] URL');
+            console.log('Get executes a HTTP GET request for a given URL.');
+            console.log(' -v Prints the detail of the response such as protocol,');
+            console.log('status, and headers.');
+            console.log(' -h key:value Associates headers to HTTP Request with the format');
+            console.log('\'key:value\'.');
+            break;
+        case "post":
+            printdefaultHelp = false;
+            console.log('usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL');
+            console.log('Post executes a HTTP POST request for a given URL with inline data or');
+            console.log('from file.');
+            console.log(' -v Prints the detail of the response such as protocol, ');
+            console.log('     status, and headers.');
+            console.log(' -h key:value Associates headers to HTTP Request with the format');
+            console.log('\'key:value\'.');
+            console.log(' -d string Associates an inline data to the body HTTP POST');
+            console.log('request.');
+            console.log(' -f file Associates the content of a file to the body HTTP');
+            console.log('POST request.');
+            console.log('Either [-d] or [-f] can be used but not both.');
+            break;
+        }
+    }
+    if (printdefaultHelp) {
+        console.log('httpc is a curl-like application but supports HTTP protocol only.');
+        console.log('Usage:');
+        console.log('httpc command [arguments]');
+        console.log('The commands are:');
+        console.log('get executes a HTTP GET request and prints the response.');
+        console.log('post executes a HTTP POST request and prints the response.');
+        console.log('help prints this screen.');
+        console.log('Use "httpc help [command]" for more information about a command.');
+    }
+    process.exit(-1);
+}
+
+
+//handle get
+if (process.argv[2].toLowerCase() == "get") {
+    console.log('node client.js get [-v] [-h key:value] URL');
+    var type = "GET";
+    var verbose = (process.argv[3].toLowerCase() == "-v");
+    var url = process.argv[process.argv.length - 1];
+    var additHeaderParams = "";
+
+    //handle the remaining -h
+    var numberParamsNoH = 4;
+    if (verbose) {
+        numberParamsNoH++;
+    }
+    //if there is not an even humber of [-h key:value] left, quit
+    if ((process.argv.length - numberParamsNoH) % 2 != 0) {
+        console.log("An invalid number of parameters was passed.")
+        process.exit(-1);
+    }
+
+    while (process.argv.length - numberParamsNoH > 0) {
+        if (process.argv[numberParamsNoH - 1].toLowerCase() != "-h") {
+            console.log("An invalid pair was specified in the -h key:value Associates headers.")
+            process.exit(-1);
+        }
+        additHeaderParams = additHeaderParams + process.argv[numberParamsNoH] + "\n";
+        numberParamsNoH++;
+        numberParamsNoH++;
+    }
+    console.log(additHeaderParams);
+
+}
+/*
+//verify the input
+if (!(process.argv[2] == 'get' || process.argv[2] == 'post')) {
+    console.log('error. try node client.js help');
+    process.exit(-1);
+}
+for (var proc in process.argv) {
+    console.log(process.argv[proc]);
+}
+console.log(process.argv);
+*/
+const client = net.createConnection({
+    host: argv.host,
+    port: argv.port
+});
 
 client.on('data', buf => {
-  console.log("-----DATA RECEIVED BACK");
+    console.log("-----DATA RECEIVED BACK");
     console.log(buf.toString());
     console.log("-----DONE");
 });
 
 client.on('connect', () => {
-    if(argv.get==1){
-        client.write("GET "+argv.path+" HTTP/1.0 \n Host: "+argv.host+"\n\n");
+    if (argv.get == 1) {
+        client.write("GET " + argv.path + " HTTP/1.0 \n Host: " + argv.host + "\naaaaa: bbbbb\n\n");
     } else {
-        client.write("POST /post HTTP/1.0\nHost: httpbin.org\ncontent-type: application/json\ncontent-length: 23\n\n{\"mapName\":\"myMapName\"}\n");
+        client.write("POST /post HTTP/1.0\nHost: httpbin.org\ncontent-type: application/json\naaaaa: bbbbb\ncontent-length: 23\n\n{\"mapName\":\"myMapName\"}\n");
     }
 });
 
 client.on('error', err => {
-  console.log('socket error %j', err);
-  process.exit(-1);
+    console.log('socket error %j', err);
+    process.exit(-1);
 });
