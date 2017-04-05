0c2696b63d0d22ae050cee4aae7841e85ed75a8b
Tyler Ramsay <starvator.ty@gmail.com>
2017-04-05 13:05:29 -0400

    broadcast

--- A3_26948065/sender.js
+++ A3_26948065/sender.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var PORT = 33333;
-var HOST = '127.0.0.1';
+var BROADCAST = '255.255.255.255';
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
+var stdin = process.openStdin();
 
 var isFirst = true;
 var name = "Guest";
@@ -12,31 +13,30 @@ var name = "Guest";
 server.on('listening', function () {
     var address = server.address();
     //console.log('UDP Server listening on ' + address.address + ":" + address.port);
-    console.log("Enter your name:");
+    console.log("Hello %s! Enter your name:",name);
 });
 
 server.on('message', function (message, remote) {
     var splitMsg = message.toString().split("\n");
     var sentName = splitMsg[0].split(":")[1];
     var sentMsg = splitMsg[1].split(":")[1];
-    console.log("%s [%s]: %s",new Date(),sentName,sentMsg);
+    console.log("%s [%s]: %s", new Date(), sentName, sentMsg);
     //console.log(remote.address + ':' + remote.port + ' - ' + message);
 
 });
 
-server.bind(PORT, HOST);
-
-var stdin = process.openStdin();
+//server.bind();
+server.bind(PORT, "", function () {
+    server.setBroadcast(true);
+});
 
 stdin.addListener("data", function (d) {
-    if (isFirst){
+    if (isFirst) {
         name = d.toString().trim();
-        console.log("Welcome to the chat %s!",name);
+        console.log("Welcome to the chat %s!", name);
         isFirst = false;
     } else {
-    var message = "user:"+name+"\nmessage:"+d.toString().trim()+"\n\n";
-    server.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
-        //console.log('UDP message sent to ' + HOST + ':' + PORT);
-    });
+        var message = "user:" + name + "\nmessage:" + d.toString().trim() + "\n\n";
+        server.send(message, 0, message.length, PORT, BROADCAST);
     }
 });
